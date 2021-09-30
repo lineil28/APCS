e8431844ee6fd5343b38c27d1b8c9b40ee0b7349
lineil28 <nlin30@stuy.edu>
2021-09-30 14:02:30 -0400

    Greet

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,10 @@
+public class Greet{
+	public static void main (String[] args){
+		Greet2("Jung")
+		Greet2("JSan")
+		Greet2("Leen")		
+  	}
+  	public static void Greet2 (String s) {
+  		System.out.println("quack quack quack quack");
+  	}
+}
