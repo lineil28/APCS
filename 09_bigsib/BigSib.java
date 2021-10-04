9a2d4529005cefb495b519ea171193dc36c20533
Neil Lin <nlin30@stuy.edu>
2021-10-04 18:53:16 -0400

    bigsib

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+/* <Kevin> (Humans: Neil, Nora, Justin; Ducks: Hatch, Hans, Alfred)
+APCS
+HW<09> -- <What Are BigSibs Good For?>
+<2021>-<10>-<2>
+*/
+
+public class BigSib {
+    public static void greet (String name) {
+  	    System.out.println("quack quack quack quack");
+  	}
+}
