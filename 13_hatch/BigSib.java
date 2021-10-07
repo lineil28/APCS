a0a0918b7e84d47ed7716e55bfadd75fc67075ce
Neil Lin <nlin30@stuy.edu>
2021-10-07 14:18:07 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -13,13 +13,15 @@ public class BigSib {
 	private String msg;
 
 	public BigSib(String phrase) {
-		setMsg(phrase);
+/*	setMsg(phrase);   */
+    msg = phrase;
 	}
 
-	public void setMsg(String phrase) {
+/*
+  public void setMsg(String phrase) {
 		msg = phrase;
 	}
-
+*/
 	public String greet(String name) {
 		return msg + " " + name;
 	}
