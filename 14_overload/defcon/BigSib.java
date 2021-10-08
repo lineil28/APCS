3069a3dc5d111f251069dbf12e825610121e9207
Neil Lin <nlin30@stuy.edu>
2021-10-08 13:03:20 -0400

    14

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,22 @@
+/* DN + Lauren (Humans: Neil, Ari, Lauren; Ducks: Hatch, Javier, (Lauren duck name))
+APCS
+HW 14 -- Customize Your Creation
+2021 10 07
+DISCO
+	You can "overload" a constructor with variations of different parameters in order to take different values of inputs.
+QCC
+
+*/
+
+public class BigSib {
+
+	private String msg;
+
+	public BigSib(String phrase) {
+		msg = phrase;
+	}
+
+	public String greet(String name) {
+		return msg + " " + name;
+	}
+}
---
+++ 14_overload/overcon/BigSib.java
@@ -0,0 +1,28 @@
+/* DN + Lauren (Humans: Neil, Ari, Lauren; Ducks: Hatch, Javier, (Lauren duck name))
+APCS
+HW 14 -- Customize Your Creation
+2021 10 07
+DISCO
+	You can "overload" a constructor with variations of different parameters in order to take different values of inputs.
+QCC
+
+*/
+
+public class BigSib {
+
+	private String msg;
+
+	public BigSib() {
+		msg = "Good morning and sun salutations";
+	}
+
+/*
+	public BigSib(String phrase) {
+		msg = phrase;
+	}
+*/
+
+	public String greet(String name) {
+		return msg + " " + name;
+	}
+}
