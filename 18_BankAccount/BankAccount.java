ce14471e88424aed336d771f8c50351241c598a2
Neil Lin <nlin30@stuy.edu>
2021-10-14 22:45:17 -0400

    bank

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,29 +1,44 @@
+/* Frog Hats (Neil, Kevin, Marcus)
+APCS
+HW 18 - CPA-One
+2021 10 14
+
+DISCO:
+
+QCC:
+
+Team xxxxx’s Latest and Greatest Q2 Response:
+
+Team xxxxx’s Latest and Greatest Q3 Response:
+
+*/
+
 public class BankAccount {
 	//instance variables
-	String private accountName, accountPassword;
-	int private pin, accountId;
-	long private accountBalance;
-	
+	private String accountName, accountPassword;
+	private int pin, accountId;
+	private long accountBalance;
+
 	public void setAccountName(String newName) {
 		accountName = newName;
 	}
-	
+
 	public void setPassword(String newPassword) {
 		accountPassword = newPassword;
 	}
-	
+
 	public void setPin(int newPin) {
 		pin = newPin;
 	}
-	
+
 	public void deposit(long value) {
 		accountBalance += value;
 	}
-	
+
 	public void withdraw(long value) {
 		accountBalance -= value;
 	}
-	
+
 	public void accountInfo() {
 		System.out.println("Account information of " + accountName + "'s account:");
 		System.out.println("Balance: " + accountBalance);
